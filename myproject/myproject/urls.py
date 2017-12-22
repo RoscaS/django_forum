b8380fb6760324033718d730ea2e8c8fa71859b5
Sol Rosca <sol.rosca@he-arc.ch>
2017-12-22 02:13:57 +0100

    styling

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -13,10 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path
-from django.conf.urls import url
 
+from accounts import views as accounts_views
 from boards import views
 
 urlpatterns = [
@@ -25,6 +26,7 @@ urlpatterns = [
     # path('', views.home, name='home'),
 
     url(r'^$', views.home, name='home'),
+    url(r'^signup/$', accounts_views.signup, name='signup'),
     url(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
     url(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
     url(r'^admin/', admin.site.urls),
